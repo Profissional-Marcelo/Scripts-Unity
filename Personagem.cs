4b436cbee421307ec7dc26bf959cc4fb179cb6fd
Yasmin-Marquet <profissional.marcelorei@gmail.com>
2025-03-10 21:46:09 -0300

    Update Personagem.cs

--- Personagem.cs
+++ Personagem.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Personagem : MonoBehaviour
 {
+
+    public int vida = 45;
+    private float forca = 1.5f;
+    private bool estaVivo = true;
+    GameObject player;
+    Rigidbody rig;
+
+
     // Start is called before the first frame update
     void Start()
     {
